16e448446179996d4997cf3ea725be03ebbefe1f
sirnet <artem@home-pc>
2020-03-22 21:45:24 +0300

    Добавлен class

---
+++ php/FormStatus.php
@@ -0,0 +1,18 @@
+<?php
+    class FormStatus
+    {
+        const STATUS_NEW = 'new';
+        const STATUS_CANSEL = 'cansel';
+        const STATUS_WORK = 'work';
+        const STATUS_PERFORMEND = 'performed';
+        const STATUS_FAILED = 'failed';
+    }
+
+    $arrayCart = [
+        'new'       =>  'Новое',
+        'cansel'    =>  'Отменено',
+        'work'      =>  'В работе',
+        'performed' =>  'Выполнено',
+        'failed'    =>  'Провалено'
+    ];
+?>
