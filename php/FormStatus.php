8f1563c21e07db6326f50b0c98a38a82304640f7
Артем Шомин <artemshomin@gmail.com>
2020-03-23 16:37:51 +0300

    Create FormStatus.php

---
+++ php/FormStatus.php
@@ -0,0 +1,20 @@
+<?php
+
+    $arrayStatus = [
+        'new'       =>  'Новое',
+        'cancel'    =>  'Отмененное',
+        'work'      =>  'В работе',
+        'performed' =>  'Выполнено',
+        'failed'    =>  'Провалено'
+    ];
+
+    class FormStatus {
+        const STATUS_NEW = 'new';
+        const STATUS_CANCEL = 'cancel';
+        const STATUS_WORK = 'work';
+        const STATUS_PERFORMED = 'performed';
+        const STATUS_FAILD = 'failed';
+    }
+
+    
+?>
