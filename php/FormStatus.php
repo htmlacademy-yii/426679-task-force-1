7cb3b5fa7a9dbea4aaa2fac829ee356e945d676d
Артем Шомин <artemshomin@gmail.com>
2020-03-25 16:49:55 +0300

    Update FormStatus.php

--- php/FormStatus.php
+++ php/FormStatus.php
@@ -1,19 +1,24 @@
 <?php
-
-    $arrayStatus = [
-        'new'       =>  'Новое',
-        'cancel'    =>  'Отмененное',
-        'work'      =>  'В работе',
-        'performed' =>  'Выполнено',
-        'failed'    =>  'Провалено'
-    ];
-
+    #Класс
     class FormStatus {
         const STATUS_NEW = 'new';
         const STATUS_CANCEL = 'cancel';
         const STATUS_WORK = 'work';
         const STATUS_PERFORMED = 'performed';
         const STATUS_FAILD = 'failed';
+
+        #Публичный метод возврата карты статуса
+        public function cardStatus() {
+            $arrayStatus = [
+                'new'       =>  'Новое',
+                'cancel'    =>  'Отмененное',
+                'work'      =>  'В работе',
+                'performed' =>  'Выполнено',
+                'failed'    =>  'Провалено'
+            ];
+
+            return null;
+        }
     }
 
     
