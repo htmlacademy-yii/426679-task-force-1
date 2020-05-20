af5d77aaeb63ae321d58f812457355d17a1fe809
sirnet <artem@home-pc>
2020-05-20 20:09:10 +0300

    Update FormStatus.php

--- php/FormStatus.php
+++ php/FormStatus.php
@@ -1,18 +1,30 @@
 <?php
-    class FormStatus
-    {
+    #Класс
+    class FormStatus {
         const STATUS_NEW = 'new';
-        const STATUS_CANSEL = 'cansel';
+        const STATUS_CANCEL = 'cancel';
         const STATUS_WORK = 'work';
-        const STATUS_PERFORMEND = 'performed';
-        const STATUS_FAILED = 'failed';
+        const STATUS_PERFORMED = 'performed';
+        const STATUS_FAILD = 'failed';
+
+        public $performerId;
+        public $customerId;
+
+        public $arrayStatus = [
+            'new'       =>  'Новое',
+            'cancel'    =>  'Отмененное',
+            'work'      =>  'В работе',
+            'performed' =>  'Выполнено',
+            'failed'    =>  'Провалено'
+        ];
+
+        #Конструктор
+        public function __construct($performerId, $customerId)
+        {
+            $this->performerId = $performerId;
+            $this->customerId = $customerId;
+        }
     }
 
-    $arrayCart = [
-        'new'       =>  'Новое',
-        'cansel'    =>  'Отменено',
-        'work'      =>  'В работе',
-        'performed' =>  'Выполнено',
-        'failed'    =>  'Провалено'
-    ];
+
 ?>
